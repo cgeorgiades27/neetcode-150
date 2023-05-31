2e4da97843364b327002c176ec8ee27fd15aa0fc
Chris Georgiades <cg@csq.dev>
2023-05-31 08:47:33 -0400

    added backtracking 01

---
+++ backtracking/001_test.go
@@ -0,0 +1,13 @@
+package backtracking
+
+// Given an integer array nums of unique elements, return all possible subsets (the power set).
+
+// The solution set must not contain duplicate subsets. Return the solution in any order.
+
+// Test case 1:
+// Input: nums = [1,2,3]
+// Output: [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
+
+// Test case 2:
+// Input: nums = [0]
+// Output: [[],[0]]
